14db37e440f92118be3e34cd872ebabd1aa1f9ca
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-05-06 13:24:00 -0600

    Corrección de la funcion del menu

--- Spring/parcial1/com/uv/jamc/clases/MenuFunc.java
+++ Spring/parcial1/com/uv/jamc/clases/MenuFunc.java
@@ -7,12 +7,15 @@ public class MenuFunc {
         switch (input) {
             case 1:
                 System.out.println("Opción Mostrar tabla");
+                opcion1();
                 break;
             case 2:
                 System.out.println("Mostrar tablas del 2 al 5");
+                opcion2();
                 break;
             case 3:
                 System.out.println("Saliendo");
+                break;
             default:
                 System.out.println("Ingrese una opción correcta");
         }
@@ -30,9 +33,13 @@ public class MenuFunc {
         Scanner input = new Scanner(System.in);
         System.out.println("Tablas del 2 al 5");
         Tablas tablas = new Tablas();
+        System.out.println("=====Tabla del 2=====");
         tablas.mostrar(2);
+        System.out.println("\n=====Tabla del 3=====");
         tablas.mostrar(3);
+        System.out.println("\n=====Tabla del 4=====");
         tablas.mostrar(4);
+        System.out.println("\n=====Tabla del 5=====");
         tablas.mostrar(5);
     }
 }
