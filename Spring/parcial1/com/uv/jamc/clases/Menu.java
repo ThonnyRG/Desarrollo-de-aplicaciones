be6f3bcd282c9b9ac63bc801b5e324a96053a865
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-05-06 13:05:57 -0600

    Added the class menu, displays the mesage in console

---
+++ Spring/parcial1/com/uv/jamc/clases/Menu.java
@@ -0,0 +1,15 @@
+package com.uv.jamc.clases;
+
+
+/* 
+ 1) Mostrar una tabla, 2) Mostrar tablas del 2 al 5, 3) Salir
+ */
+public class Menu {
+    public void msgMenu(){
+        System.out.println("===MENU===");
+        System.out.println("Ingresa una opción");
+        System.out.println("1.-Mostrar tabla");
+        System.out.println("2.-Mostrar tablas del 2 al 5");
+        System.out.println("3.-Salir");
+    }
+}
